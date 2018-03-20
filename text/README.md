f5dd77d8edbb68ed8b122e24921b56ad1544e4bd
ddua <dheeru_dua@hotmail.com>
2018-03-20 11:55:07 -0700

    updating readme and checking in baseline file

--- text/README.md
+++ text/README.md
@@ -1,67 +1,27 @@
 ## Experiment on Text
 
-Code is based on [Zhao et al., 2017](https://github.com/jakezhaojb/ARAE)
+Code is: based on [Zhao et al., 2017](https://github.com/jakezhaojb/ARAE)
 
-### Requirements
-- PyTorch, JSON, Argparse
-- (Optional) KenLM (https://github.com/kpu/kenlm)
+### The code for text is not properly ready for use with pre-trained models yet. We will try to get to it soon. ####
 
-## Pretrained Version
+If you would still like to create the models at your end you can do so by
+0.) Download data files from 
+https://drive.google.com/file/d/1qOA3P3krBFI6KyfTEPxAmrsaUEC6y-_G/view?usp=sharing
 
-1) Download and unzip https://drive.google.com/drive/folders/0B4IZ6lmAKTWJSE9UNFYzUkphaVU?usp=sharing.
+1.) Training the baseline modeline 
+python train_baseline.py --data_path <path> --save_path <model_save_path>
 
-2) Run: 
+2.) [Optional] Getting the pretrained models for autoencoder, generator and discriminator
+https://drive.google.com/file/d/14NNZRN1UOB0jLCfcuQHYDMJaG9bfYQwE/view?usp=sharing
 
-    `python generate.py --load_path ./maxlen30`
+3.) Train an inverter
+You can either use the pretrained models in step 2 or train your own models with 
+python train.py --data_path <path> --update_base --convolution_enc 
+Once you have the trained models for autoencoder, generator and discriminator, you can train the inverter as below
+python train.py --data_path <path> --load_pretrained <dir_name_with_respect_to_model_saved_location>
 
-    (Requires CUDA and Python)
-
-
-
-## Data Preparation
-
-### SNLI Data Preparation
-- Download dataset and unzip: https://nlp.stanford.edu/projects/snli/snli_1.0.zip
-- Run `python snli_preprocessing.py --in_path PATH_TO_SNLI --out_path PATH_TO_PROCESSED_DATA`
-    - Example: `python snli_preprocessing.py --in_path ../Data/snli_1.0 --out_path ../Data/snli_lm`
-    - The script will create the output directory if it doesn't already exist
-- For more information on SNLI see: https://nlp.stanford.edu/projects/snli/
-
-### Your Customized Datasets
-If you would like to train a text ARAE on another dataset, simply
-1) Create a data directory with a `train.txt` and `test.txt` files with line delimited sentences.
-2) Run training command with the `--data_path` argument pointing to that data directory.
-
-## Train
-1) To train without KenLM: 
-
-    `python train.py --data_path PATH_TO_PROCESSED_DATA --cuda --no_earlystopping`
-
-2) To train with KenLM for early stopping: 
-
-    `python train.py --data_path PATH_TO_PROCESSED_DATA --cuda --kenlm_path PATH_TO_KENLM_DIRECTORY`
-
-- When training on default parameters the training script will output the logs, generations, and saved models to: `./output/example`
-
-### Model Details
-- We train on sentences that have up to 30 tokens and take the most likely word (argmax) when decoding (there is an option to sample when decoding as well).
-- For a numerical way for early stopping, after the model has trained for a specified minimum number of epochs, we periodically train a n-gram language model (with modified Kneser-Ney and Laplacian smoothing) on 100,000 generated sentences and evaluate the perplexity of real sentences from a held-out test set. If the perplexity does not improve over that of the lowest perplexity seen for a certain number of iterations (patience), we end training.
+The TreeLSTM and the machine translation results in the paper were done offline and are not included in the respository. If you wish to get those results you can contact ddua@uci.edu for further details
 
 
-### KenLM Installation:
-- Download stable release and unzip: http://kheafield.com/code/kenlm.tar.gz
-- Need Boost >= 1.42.0 and bjam
-    - Ubuntu: `sudo apt-get install libboost-all-dev`
-    - Mac: `brew install boost; brew install bjam`
-- Run within kenlm directory:
-    ```bash
-    mkdir -p build
-    cd build
-    cmake ..
-    make -j 4
-    ```
-- `pip install https://github.com/kpu/kenlm/archive/master.zip`
-- For more information on KenLM see: https://github.com/kpu/kenlm and http://kheafield.com/code/kenlm/
-
-
-
+Acknowledgments:
+Thanks to jakezhaojb for his excellent code on the ARAE model.
---
+++ text/train_baseline.py
@@ -0,0 +1,117 @@
+'''
+Created on Oct 1, 2017
+
+@author: dheeru
+'''
+import argparse
+from models import Baseline_Embeddings, Baseline_LSTM
+from utils import to_gpu, Corpus, batchify, SNLIDataset, collate_snli
+import random
+import torch
+import numpy as np
+import torch.optim as optim
+import torch.nn as nn
+import torch.utils.data
+from torch.autograd import Variable
+
+def evaluate_model():
+    test_iter = iter(testloader)
+    correct=0
+    total=0
+    for batch in test_iter:
+        premise, hypothesis, target, _, _, _ = batch
+        
+        if args.cuda:
+            premise=premise.cuda()
+            hypothesis = hypothesis.cuda()
+            target = target.cuda()
+            
+        prob_distrib = baseline_model.forward((premise, hypothesis))
+        predictions = np.argmax(prob_distrib.data.cpu().numpy(), 1)
+        correct+=len(np.where(target.data.cpu().numpy()==predictions)[0])
+        total+=premise.size(0)
+    acc=correct/float(total)
+    print("Accuracy:{0}".format(acc))
+    return acc
+        
+        
+parser = argparse.ArgumentParser(description='PyTorch baseline for Text')
+parser.add_argument('--data_path', type=str, required=True,
+                    help='location of the data corpus')
+parser.add_argument('--model_type', type=str, default="emb",
+                    help='location of the data corpus')
+parser.add_argument('--epochs', type=int, default=20,
+                    help='maximum number of epochs')
+parser.add_argument('--batch_size', type=int, default=32, metavar='N',
+                    help='batch size')
+parser.add_argument('--packed_rep', type=bool, default=True,
+                    help='pad all sentences to fixed maxlen')
+parser.add_argument('--train_mode', type=bool, default=True,
+                    help='set training mode')
+parser.add_argument('--maxlen', type=int, default=10,
+                    help='maximum sentence length')
+parser.add_argument('--lr', type=float, default=1e-05,
+                    help='learning rate')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='seed')
+parser.add_argument('--beta1', type=float, default=0.9,
+                    help='beta1 for adam. default=0.9')
+parser.add_argument('--cuda', action='store_true', default=True,
+                    help='use CUDA')
+parser.add_argument('--save_path', action='store_true', default=True,
+                    help='used for saving the models')
+
+args = parser.parse_args()
+
+corpus_train = SNLIDataset(train=True, vocab_size=11004, lvt=False, path=args.data_path)
+corpus_test = SNLIDataset(train=False, vocab_size=11004, lvt=False, path=args.data_path)
+trainloader= torch.utils.data.DataLoader(corpus_train, batch_size = args.batch_size, collate_fn=collate_snli, shuffle=True)
+train_iter = iter(trainloader)
+testloader= torch.utils.data.DataLoader(corpus_test, batch_size = args.batch_size, collate_fn=collate_snli, shuffle=False)
+
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+
+if args.model_type=="lstm":
+    baseline_model = Baseline_LSTM(100,300,maxlen=args.maxlen, gpu=args.cuda)
+elif args.model_type=="emb":
+    baseline_model = Baseline_Embeddings(100,maxlen=args.maxlen, gpu=args.cuda, vocab_size=11004)
+    
+if args.cuda:
+    baseline_model = baseline_model.cuda()
+optimizer = optim.Adam(baseline_model.parameters(),
+                           lr=args.lr,
+                           betas=(args.beta1, 0.999))
+criterion = nn.CrossEntropyLoss()
+
+best_accuracy = 0
+if args.train_mode:
+    for epoch in range(0, args.epochs):
+        niter = 0
+        loss_total = 0
+        while niter < len(trainloader):
+            niter+=1
+            premise, hypothesis, target = train_iter.next()
+            if args.cuda:
+                premise=premise.cuda()
+                hypothesis = hypothesis.cuda()
+                target = target.cuda()
+            prob_distrib = baseline_model.forward((premise, hypothesis))
+            loss=criterion(prob_distrib, target)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            loss_total += loss.data[0]
+        print(loss_total/float(niter))
+        train_iter = iter(trainloader)
+        curr_acc = evaluate_model()
+        if curr_acc > best_accuracy:
+            print("saving model...")
+            with open(args.save_path+"/"+args.model_type+'.pt', 'wb') as f:
+                torch.save(baseline_model.state_dict(), f)
+            best_accuracy = curr_acc
+        
+    print("Best accuracy :{0}".format(best_accuracy))
+
+        
